9f20a9c1009e5b76c30d86d0c2e9e02677353c8d
chojayoung <26006@naver.com>
2024-11-09 17:27:22 +0900

    Modify : solidity 코드 수정

--- truffle-project/contracts/MyContract.sol
+++ truffle-project/contracts/MyContract.sol
@@ -2,15 +2,45 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
+    string public myString = 'hello world !';
+    struct NFT {
+        uint256 id;
+        string metadata;
+        string imageUrl;
+        address owner;
+    }
+    NFT[] public nfts;
+    uint256 public nextNFTId = 1;
 
-  string public myString = 'hello world !';
+    function setMyString(string memory _myString) public {
+        myString = _myString;
+    }
 
-  function setMyString (string memory _myString) public {
-    myString = _myString;
-  }
+    function getMyString() public view returns(string memory) {
+        return myString;
+    }
 
-  function getMyString() public view returns(string memory){
-    return myString;
-  }
+    function mintNFT(string memory metadata, string memory imageUrl) public {
+        NFT memory newNFT = NFT(nextNFTId, metadata, imageUrl, msg.sender);
+        nfts.push(newNFT);
+        nextNFTId++;
+    }
 
+    function getNFTsByOwner(address owner) public view returns (NFT[] memory) {
+        uint256 count = 0;
+        for (uint256 i = 0; i < nfts.length; i++) {
+            if (nfts[i].owner == owner) {
+                count++;
+            }
+        }
+        NFT[] memory result = new NFT[](count);
+        uint256 index = 0;
+        for (uint256 i = 0; i < nfts.length; i++) {
+            if (nfts[i].owner == owner) {
+                result[index] = nfts[i];
+                index++;
+            }
+        }
+        return result;
+    }
 }
